9b4d5bad64053143f482ae531f4accfa187d75d2
zailongshi <zailongshi2013@gmail.com>
2019-07-17 00:20:28 +0800

    FragmentHost add new feature

--- app/src/main/java/com/mylive/live/widget/FragmentHost.java
+++ app/src/main/java/com/mylive/live/widget/FragmentHost.java
@@ -8,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Create by zailongshi on 2019/7/15
  */
@@ -17,6 +20,8 @@ public final class FragmentHost {
     private Fragment selectedFragment;
     @IdRes
     private int fragmentContainerId;
+    private boolean keep, remove;
+    private List<Class<? extends Fragment>> willBeRemovedFragments;
 
     public static FragmentHost create(FragmentManager fragmentManager,
                                       @IdRes int fragmentContainerId) {
@@ -29,6 +34,24 @@ public final class FragmentHost {
         this.fragmentContainerId = fragmentContainerId;
     }
 
+    public FragmentHost keep() {
+        keep = true;
+        return this;
+    }
+
+    public FragmentHost remove() {
+        remove = true;
+        return this;
+    }
+
+    public FragmentHost remove(Class<? extends Fragment> fragment) {
+        if (willBeRemovedFragments == null) {
+            willBeRemovedFragments = new ArrayList<>();
+        }
+        willBeRemovedFragments.add(fragment);
+        return this;
+    }
+
     public void show(Context context, Class<? extends Fragment> fragment, Bundle args) {
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         String fragmentName = fragment.getName();
@@ -37,11 +60,26 @@ public final class FragmentHost {
             fragment1 = Fragment.instantiate(context, fragmentName, args);
             transaction.add(fragmentContainerId, fragment1, fragmentName);
         }
-        if (selectedFragment != null) {
+        for (Class<? extends Fragment> willBeRemovedFragment : willBeRemovedFragments) {
+            String fragmentName2 = willBeRemovedFragment.getName();
+            Fragment fragment2 = fragmentManager.findFragmentByTag(fragmentName2);
+            if (fragment2 != null) {
+                transaction.remove(fragment2);
+            }
+        }
+        boolean selectedFragmentRemoved = willBeRemovedFragments != null
+                && willBeRemovedFragments.contains(selectedFragment.getClass());
+        if (remove && selectedFragment != null
+                && !selectedFragmentRemoved) {
+            transaction.remove(selectedFragment);
+        } else if (!keep && selectedFragment != null
+                && !selectedFragmentRemoved) {
             transaction.hide(selectedFragment);
         }
         transaction.show(fragment1).commitAllowingStateLoss();
         selectedFragment = fragment1;
+        remove = false;
+        keep = false;
     }
 
     public void show(Context context, Class<? extends Fragment> fragment) {
